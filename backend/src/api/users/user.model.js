b5e755c9e07eac3f5b371d46bf6144adb2e7d9fd
Marina-Melikhova <marinamelihova29@gmail.com>
2020-11-13 23:45:57 +0200

    added timestamps

--- backend/src/api/users/user.model.js
+++ backend/src/api/users/user.model.js
@@ -1,28 +1,26 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const UserSchema = new Schema({
-  username: { type: String, required: true },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
+const UserSchema = new Schema(
+  {
+    username: { type: String, required: true },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    passwordHash: { type: String },
+    balance: { type: Number, default: 0, required: true },
+    flatPrice: { type: Number, default: 0, required: true },
+    flatSquareMeters: { type: Number, default: 0, required: true },
+    totalSalary: { type: Number, default: 0, required: true },
+    passiveIncome: { type: Number, default: 0, required: true },
+    incomePercentageToSavings: { type: Number, default: 0, required: true },
+    giftsUnpacked: { type: Number, default: 0 },
+    giftsForUnpacking: { type: Number, default: 0 },
+    tokens: [{ type: String }],
   },
-  passwordHash: { type: String },
-  balance: { type: Number, default: 0, required: true },
-  flatPrice: { type: Number, default: 0, required: true },
-  flatSquareMeters: { type: Number, default: 0, required: true },
-  totalSalary: { type: Number, default: 0, required: true },
-  passiveIncome: { type: Number, default: 0, required: true },
-  incomePercentageToSavings: { type: Number, default: 0, required: true },
-  giftsUnpacked: { type: Number, default: 0 },
-  giftsForUnpacking: { type: Number, default: 0 },
-  tokens: [{ type: String }],
-  createdAt: {
-    type: Date,
-    default: new Date(),
-  },
-  updatedAt: Date,
-});
+  { timestamps: true },
+);
 
 module.exports = mongoose.model('User', UserSchema);
