8d87d3e7cd70c7455a4a67574213e591057c176a
Pa30p42 <hajiev93@gmail.com>
2020-11-14 13:58:58 +0300

    started

---
+++ backend/src/utils.js/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const UserModel = require('../auth/user.model');
+const AppError = require('./AppError');
+
+exports.authorize = async (req, res, next) => {
+  const authHeader = req.get('Authorization');
+  if (!authHeader) {
+    return next(new AppError('Unauthorized', 401));
+  }
+
+  const token = authHeader.replace('Bearer ', '');
+  let userId;
+  try {
+    userId = jwt.verify(token, process.env.JWT_SECRET).id;
+  } catch (error) {
+    next(new AppError('Unauthorized', 401));
+  }
+  const user = await UserModel.findById(userId);
+  if (!user) {
+    next(new AppError('Unauthorized', 401));
+  }
+  req.user = user;
+  next();
+};
