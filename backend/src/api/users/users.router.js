f7d2979623b5abd0023c39fa802b6334d877d3f0
nyuta <pillowforneedles@gmail.com>
2020-11-15 18:19:42 +0200

    validation for update user

--- backend/src/api/users/users.router.js
+++ backend/src/api/users/users.router.js
@@ -2,7 +2,13 @@ const { Router } = require('express');
 const { updateUsersController } = require('./users.controllers');
 const usersRouter = Router();
 const { authorize } = require('../../utils.js/authMiddleware');
+const { userValidationMiddleware } = require('./users.middleware');
 
-usersRouter.put('/savings-info', authorize, updateUsersController);
+usersRouter.put(
+  '/savings-info',
+  authorize,
+  userValidationMiddleware,
+  updateUsersController,
+);
 
 module.exports = usersRouter;
