50eee1ac7e9b075d8e4dfc7df25b7f5ba2030b03
Marina Sergeyeva <morrrinad@gmail.com>
2020-11-16 17:00:33 +0200

    dynamics page done

--- frontend/src/pages/DynamicsPage/DynamicsPage.js
+++ frontend/src/pages/DynamicsPage/DynamicsPage.js
@@ -1,7 +1,111 @@
 import React from 'react';
+import styled from 'styled-components'
+import device, { Mobile, Tablet, Desktop } from '../../common/deviceSizes';
+import { colors } from '../../stylesheet/vars'
+
 
 const DynamicsPage = () => {
-  return <p>'DynamicsPage'</p>;
+  return <>
+    {/* <p>'DynamicsPage'</p> */}
+    <DynamicsPageWrapper>
+      <GraphAnnualWrapper>
+        <GrafWrapper>
+          <p>'Graph'</p>
+        </GrafWrapper>
+        <AnnualWrapper>
+          <p>'Annual'</p>
+        </AnnualWrapper>
+      </GraphAnnualWrapper>
+      <ProgressPicturePresentWrapper>
+        <ProgressPictureWrapper>
+          <ProgressInfoWrapper>
+            <p>'ProgressInfo'</p>
+          </ProgressInfoWrapper>
+          <PictureWrapper>
+            <p>'Picture'</p>
+          </PictureWrapper>
+        </ProgressPictureWrapper>
+      <PresentWrapper>
+          <p>'Present'</p>
+      </PresentWrapper>
+      </ProgressPicturePresentWrapper>
+    </DynamicsPageWrapper>
+    </>
 };
 
 export default DynamicsPage;
+
+const DynamicsPageWrapper = styled.div`
+padding: 38px 20px 56px;
+
+@media ${device.tablet} {
+  padding: 74px 130px;
+}
+@media ${device.desktop} {
+  display: flex;
+  justify-content: space-between;
+  padding: 60px 56px 64px;
+}
+`;
+
+const GraphAnnualWrapper = styled.div`
+`;
+
+const ProgressPicturePresentWrapper = styled.div``;
+
+const GrafWrapper = styled.div`
+  margin-bottom: 30px;
+
+  @media ${device.tablet} {
+  margin-bottom: 50px;
+  }
+`;
+
+const AnnualWrapper = styled.div`
+margin-bottom: 30px;
+
+@media ${device.tablet} {
+  margin-bottom: 75px;
+}
+@media ${device.desktop} {
+  margin-bottom: 0px;
+}
+`;
+
+const ProgressPictureWrapper = styled.div`
+@media ${device.tablet} {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 72px;
+  }
+@media ${device.desktop} {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 60px;
+  }
+`;
+
+const ProgressInfoWrapper = styled.div`
+  margin-bottom: 24px;
+
+  @media ${device.tablet} {
+  margin-bottom: 0px;
+  }
+  @media ${device.desktop} {
+  margin-bottom: 0px;
+  }
+`;
+
+const PictureWrapper = styled.div`
+  margin-bottom: 42px;
+
+    @media ${device.tablet} {
+  margin-bottom: 0px;
+  }
+    @media ${device.desktop} {
+  margin-bottom: 0px;
+  }
+`;
+
+const PresentWrapper = styled.div`
+`;
