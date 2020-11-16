95e94620a891c1c9bd4ba79caa8321b54590f4cd
Marina Sergeyeva <morrrinad@gmail.com>
2020-11-16 09:15:10 +0200

    save

---
+++ frontend/src/stylesheet/vars.js
@@ -0,0 +1,40 @@
+// --main-color: #ff6c00;
+//   --secondary-color: #7c9af2;
+//   --third-color: #d7d8dd;
+//   --main--text-color: #18191f;
+//   --secondary--text-color: rgba(24, 25, 31, 0.56);
+//   --main__box-shadow: 0px 24px 38px rgba(0, 0, 0, 0.14),
+//     0px 9px 46px rgba(0, 0, 0, 0.12), 0px 11px 15px rgba(0, 0, 0, 0.2);
+//   --main-background-color: rgba(0, 0, 0, 0.03);
+//   --secondary-background-color: #373b53;
+//   --third-background-color: rgba(55, 59, 83, 0.2);
+
+//   --main-font:'Roboto', Arial, sans-serif;
+
+const colors = {
+    main: '#ff6c00',
+    secondary: '#7c9af2',
+    third:'#d7d8dd',
+}
+
+const background = {
+    main: 'rgba(0, 0, 0, 0.03)',
+    secondary: '#373b53',
+    third:'rgba(55, 59, 83, 0.2)'
+}
+
+const textColor = {
+    main: '#18191f',
+    secondary:'rgba(24, 25, 31, 0.56)'
+}
+
+const boxShadow = {
+    main:'0px 24px 38px rgba(0, 0, 0, 0.14),0px 9px 46px rgba(0, 0, 0, 0.12), 0px 11px 15px rgba(0, 0, 0, 0.2)'
+}
+
+const fonts = {
+    main: `Roboto', Arial, sans-serif`,
+    secondary: `'Gilroy', Arial, sans-serif`
+}
+
+export {colors,background,textColor,boxShadow,fonts}
