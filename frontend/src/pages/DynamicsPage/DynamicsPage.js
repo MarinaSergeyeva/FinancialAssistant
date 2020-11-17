120faf7972b9fb0d04d4ed0eee0a18b8677f143f
Pa30p42 <hajiev93@gmail.com>
2020-11-17 14:43:44 +0300

    befoe merge

--- frontend/src/pages/DynamicsPage/DynamicsPage.js
+++ frontend/src/pages/DynamicsPage/DynamicsPage.js
@@ -1,56 +1,57 @@
 import React from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
 import device, { Mobile, Tablet, Desktop } from '../../common/deviceSizes';
-import { colors } from '../../stylesheet/vars'
-import GiftCompleting from '../../components/GiftCompleting/GiftCompleting'
-
+import { colors } from '../../stylesheet/vars';
+import GiftCompleting from '../../components/GiftCompleting/GiftCompleting';
+import ApartmentVisualization from '../../components/ApartmentVisualization/ApartmentVisualization';
 
 const DynamicsPage = () => {
-  return <>
-    {/* <p>'DynamicsPage'</p> */}
-    <DynamicsPageWrapper>
-      <GraphAnnualWrapper>
-        <GrafWrapper>
-          <p>'Graph'</p>
-        </GrafWrapper>
-        <AnnualWrapper>
-          <p>'Annual'</p>
-        </AnnualWrapper>
-      </GraphAnnualWrapper>
-      <ProgressPicturePresentWrapper>
-        <ProgressPictureWrapper>
-          <ProgressInfoWrapper>
-            <p>'ProgressInfo'</p>
-          </ProgressInfoWrapper>
-          <PictureWrapper>
-            <p>'Picture'</p>
-          </PictureWrapper>
-        </ProgressPictureWrapper>
-      <PresentWrapper>
-          <GiftCompleting />
-      </PresentWrapper>
-      </ProgressPicturePresentWrapper>
-    </DynamicsPageWrapper>
+  return (
+    <>
+      {/* <p>'DynamicsPage'</p> */}
+      <DynamicsPageWrapper>
+        <GraphAnnualWrapper>
+          <GrafWrapper>
+            <p>'Graph'</p>
+          </GrafWrapper>
+          <AnnualWrapper>
+            <p>'Annual'</p>
+          </AnnualWrapper>
+        </GraphAnnualWrapper>
+        <ProgressPicturePresentWrapper>
+          <ProgressPictureWrapper>
+            <ProgressInfoWrapper>
+              <p>'ProgressInfo'</p>
+            </ProgressInfoWrapper>
+            <PictureWrapper>
+              <ApartmentVisualization />
+            </PictureWrapper>
+          </ProgressPictureWrapper>
+          <PresentWrapper>
+            <GiftCompleting />
+          </PresentWrapper>
+        </ProgressPicturePresentWrapper>
+      </DynamicsPageWrapper>
     </>
+  );
 };
 
 export default DynamicsPage;
 
 const DynamicsPageWrapper = styled.div`
-padding: 38px 20px 56px;
+  padding: 38px 20px 56px;
 
-@media ${device.tablet} {
-  padding: 74px 130px;
-}
-@media ${device.desktop} {
-  display: flex;
-  justify-content: space-between;
-  padding: 60px 56px 64px;
-}
+  @media ${device.tablet} {
+    padding: 74px 130px;
+  }
+  @media ${device.desktop} {
+    display: flex;
+    justify-content: space-between;
+    padding: 60px 56px 64px;
+  }
 `;
 
-const GraphAnnualWrapper = styled.div`
-`;
+const GraphAnnualWrapper = styled.div``;
 
 const ProgressPicturePresentWrapper = styled.div``;
 
@@ -58,31 +59,31 @@ const GrafWrapper = styled.div`
   margin-bottom: 30px;
 
   @media ${device.tablet} {
-  margin-bottom: 50px;
+    margin-bottom: 50px;
   }
 `;
 
 const AnnualWrapper = styled.div`
-margin-bottom: 30px;
+  margin-bottom: 30px;
 
-@media ${device.tablet} {
-  margin-bottom: 75px;
-}
-@media ${device.desktop} {
-  margin-bottom: 0px;
-}
+  @media ${device.tablet} {
+    margin-bottom: 75px;
+  }
+  @media ${device.desktop} {
+    margin-bottom: 0px;
+  }
 `;
 
 const ProgressPictureWrapper = styled.div`
-@media ${device.tablet} {
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 72px;
+  @media ${device.tablet} {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 72px;
   }
-@media ${device.desktop} {
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 60px;
+  @media ${device.desktop} {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 60px;
   }
 `;
 
@@ -90,23 +91,22 @@ const ProgressInfoWrapper = styled.div`
   margin-bottom: 24px;
 
   @media ${device.tablet} {
-  margin-bottom: 0px;
+    margin-bottom: 0px;
   }
   @media ${device.desktop} {
-  margin-bottom: 0px;
+    margin-bottom: 0px;
   }
 `;
 
 const PictureWrapper = styled.div`
   margin-bottom: 42px;
 
-    @media ${device.tablet} {
-  margin-bottom: 0px;
+  @media ${device.tablet} {
+    margin-bottom: 0px;
   }
-    @media ${device.desktop} {
-  margin-bottom: 0px;
+  @media ${device.desktop} {
+    margin-bottom: 0px;
   }
 `;
 
-const PresentWrapper = styled.div`
-`;
+const PresentWrapper = styled.div``;
