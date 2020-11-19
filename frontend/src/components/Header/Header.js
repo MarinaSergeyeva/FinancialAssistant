a8479a60e0ff09c440949e4b12b1082edd1797f2
OlegKhodakivskyi <oleghodakovsky@gmail.com>
2020-11-19 10:44:18 +0200

    19/11 - 10:44 progress

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,54 +1,16 @@
-import React from 'react';
-import styled from 'styled-components';
-import device, { Mobile, Tablet, Desktop } from '../../common/deviceSizes';
-import Navigation from '../Header/Navigation';
-import Logo from '../Header/Logo';
+import React, { useState } from 'react';
+import Unauthorized from '../Header/Unauthorized';
+import Authorized from '../Header/Authorized';
 
 const Header = () => {
-  // const [state, setstate] = useState(initialState)
   // из глобального стейта  взять токен.
+  // const [token, setToken] = useState("")
   return (
     <>
-      <HeaderContainer>
-        <Logo />
-        <p>Finance</p>
-        <Navigation />
-      </HeaderContainer>
+      {/* <Unauthorized /> */}
+      <Authorized />
     </>
   );
 };
 
 export default Header;
-
-const HeaderContainer = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  padding-top: 16px;
-  padding-bottom: 16px;
-  box-shadow: 0px 1px 0px #e5e9f2;
-
-  @media ${device.tablet} {
-    width: 768px;
-    margin: 0 auto;
-  }
-  @media ${device.desktop} {
-    position: relative;
-    width: 1280px;
-    margin: 0 auto;
-  }
-
-  & p {
-    padding-top: 8px;
-    display: block;
-    min-width: 74px;
-    height: auto;
-    font-family: 'Gilroy';
-    font-style: normal;
-    font-weight: 800;
-    font-size: 20px;
-    line-height: 20px;
-    color: #18191f;
-    margin-left: 8px;
-    margin-bottom: 20px;
-  }
-`;
--- frontend/src/components/Header/Logo.js
+++ frontend/src/components/Header/Logo.js
@@ -1,41 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
-import device, { Mobile, Tablet, Desktop } from '../../common/deviceSizes';
-import logoImg from "../../assets/images/logo/logo.png"
+import logoImg from '../../assets/images/Logo/logo.png';
+import { Link } from 'react-router-dom';
 
 const Logo = () => {
   return (
-      <>
+    <>
       <LogoContainer>
-         <LogoContainerImg
-           src={logoImg}
-           alt="logo img"
-         >
-          </LogoContainerImg>
-        </LogoContainer>
-      </>
-    );
+        <Link to="/">
+          <LogoContainerImg src={logoImg} alt="logo img"></LogoContainerImg>
+        </Link>
+      </LogoContainer>
+    </>
+  );
 };
 
 export default Logo;
 
 const LogoContainer = styled.div`
-width: 26px;
-height:26px;
-padding-top:8px;
-margin-left:20px;
-margin-bottom:18px;
-
-@media ${device.tablet} {
-  margin-left:40px;
-  margin-bottom:24px;
-  }
-
-@media ${device.desktop} {
-  margin-left:55px;
-  }
+  width: 26px;
+  height: 26px;
+  padding-top: 8px;
 `;
 
 const LogoContainerImg = styled.img`
-background-size: cover;
+  background-size: cover;
 `;
