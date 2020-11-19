554ab2f4c7205b356c8448ff5470e2425135f373
Anastasia-Dmv <Vesta50000@gmail.com>
2020-11-19 19:13:24 +0200

    some

--- backend/src/api/cron/cronUpdateInfo.js
+++ backend/src/api/cron/cronUpdateInfo.js
@@ -14,16 +14,27 @@ const monthReportModel = require("./monthReport.model");
 const updateInfo = () => {
   cron.schedule("*/10 * * * * *", async () => {
     // let t = UserDB.find({ [id]: _id });
+    //const userTransactions = TransactionModel.find({ userId: user._id });
 
     const users = await UserDB.find();
 
     await Promise.all(
       users.map(async (user) => {
         const income = user.totalSalary + user.passiveIncome;
-          const totalExpansesMonthly = ;
+        let now = new Date();
+        const currentMonth = now.getMonth();
+        const userTransactions = TransactionModel.find({
+          userId: user._id,
+          $expr: {
+            $eq: [{ $month: "$transactionDate" }, currentMonth],
+          },
+        });
 
-          
-          user.balance += income;
+        const getAmount = userTransactions.reduce(function (sum, transaction) {
+          return sum + transaction.amount;
+        }, 0);
+
+        user.balance += income;
         await user.save();
 
         await TransactionModel.create({
@@ -31,18 +42,72 @@ const updateInfo = () => {
           transactionDate: new Date(),
           type: "INCOME",
         });
-        //=======
+
         await monthReportModel.create({
-          totalExpenses: {},
-          totalSavings: {},
-          totalIncome: {income},
-          expectedSavingsPercentage: {},
-          expectedSavings: {},
+          totalExpenses: getAmount(),
+          totalSavings: 200,
+          totalIncome: 200,
+          expectedSavingsPercentage: 5,
+          expectedSavings: 100,
         });
-        //========
+        // await monthReportModel.create({
+        //   totalExpenses: { monthlyExpanses },
+        //   totalSavings: monthlyIncome - monthlyExpanses,
+        //   totalIncome: { income }, //DONE
+        //   expectedSavingsPercentage: user.incomePercentageToSavings,
+        //   expectedSavings: user.flatPrice - user.totalSavings,
+        // });
       })
     );
   });
 };
-
 module.exports = { updateInfo };
+
+// const totalExpansesMonthly = userTransactions.amount;
+// console.log("userTransactions", userTransactions);
+//=======
+//   [
+//     {
+//       $project: {
+//         doc: "$$ROOT",
+//         year: { $year: "$created" },
+//         month: { $month: "$created" },
+//         day: { $dayOfMonth: "$created" },
+//       },
+//     },
+//     { $match: { month: 4, year: 2016 } },
+//     ]
+//======
+//       [
+//   { $match: { userId: user._id } },
+//   {
+//     $group: { type: "EXPENSE" },
+//     $month: { $month: "$transactionDate" },
+//   },
+//       ]
+//=======
+// await monthReportModel.create({
+//   totalExpenses: { monthlyExpanses },
+//   totalSavings: monthlyIncome - monthlyExpanses,
+//   totalIncome: { income }, //DONE
+//   expectedSavingsPercentage: user.incomePercentageToSavings,
+//   expectedSavings: user.flatPrice - user.totalSavings,
+// });
+
+// const userTransactionsfilteredMonth = userTransactions.filter(
+//   (transaction) => transaction.transactionDate. === currentMonth
+// );
+
+// await TransactionModel.aggregate([
+//   {
+//     $group: {
+//       userId: user._id,
+//       $match: {
+//         $expr: {
+//           $eq: [{ $month: "$transactionDate" }, currentMonth],
+//         },
+//       },
+//     },
+//   },
+// ]);
+// })
