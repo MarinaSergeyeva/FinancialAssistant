b022fe35814d7cb38e302b5cb991f6a474b134be
Kostya Polishko <kost927@gmail.com>
2020-11-19 19:44:39 +0200

    before pull

---
+++ backend/tests/transactionCategory.spec.js
@@ -0,0 +1,74 @@
+require('dotenv').config({ path: './.env' });
+const jwt = require('jsonwebtoken');
+const { CrudServer } = require('../src/server');
+const request = require('supertest');
+const { assert } = require('chai');
+const User = require('../src/api/users/user.model');
+
+describe('transactions/categories test suite', () => {
+  let server;
+  before(async () => {
+    const crudServer = new CrudServer();
+    await crudServer.startForTest();
+    server = crudServer.server;
+  });
+
+  describe('GET /transactions/categories', () => {
+    context('wrong token provided', () => {
+      let response;
+
+      before(async () => {
+        response = await request(server)
+          .post('/api/v1/transactions')
+          .set('Authorization', 'Bearer wrong_token');
+      });
+
+      it('should return 401 error', () => {
+        assert.equal(response.status, 401);
+      });
+     
+      context('when good token was provided', () => {
+          let response, userDoc, categories
+
+        before(async () => {
+          userDoc = await User.create({
+            username: 'Test1',
+            email: 'test1@email.com',
+            passwordHash: 'password_hash',
+          });
+
+          categories = [
+            'Другое',
+            'Развлечения',
+            'Продукты',
+            'Товары',
+            'Транспорт',
+            'ЖКХ',
+          ];
+
+          const token = jwt.sign({ id: userDoc._id }, process.env.JWT_SECRET, {
+            expiresIn: 2 * 24 * 60 * 60,
+          });
+          userDoc.tokens.push(token);
+          await userDoc.save();
+
+          response = await request(server)
+            .get('/api/v1/users/current')
+            .set('Authorization', `Bearer ${token}`);
+        });
+
+        after(async () => {
+          await User.deleteOne({ _id: userDoc._id });
+        });
+
+        it('should return response with 200', () => {
+          assert.equal(response.status, 200);
+        });
+
+          it('should return expected categories array', () => {
+          categories;
+        });
+      });
+    });
+  });
+});
