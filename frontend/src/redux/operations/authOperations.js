e62e63e83d4134d6d9aabc3b2afdfc88f2b44337
Marina Sergeyeva <morrrinad@gmail.com>
2020-11-20 16:20:42 +0200

    heroku deployed

--- frontend/src/redux/operations/authOperations.js
+++ frontend/src/redux/operations/authOperations.js
@@ -1,7 +1,8 @@
 import authAction from '../actions/authAction';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8080';
+// axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = 'http://financial-assistant-bc22.herokuapp.com';
 
 export const token = {
   set(token) {
