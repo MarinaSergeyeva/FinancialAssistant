065eea802e182b2b2a496c2d0094ec8e8a4d674b
Marina Sergeyeva <morrrinad@gmail.com>
2020-11-23 17:43:32 +0200

    save

--- backend/src/api/monthReports/monthReport.controller.js
+++ backend/src/api/monthReports/monthReport.controller.js
@@ -5,8 +5,7 @@ const getMonthReport = catchAsync(async (req, res, next) => {
   console.log('Hi from getReport');
   console.log('req.query', req.query);
   const { startMonth, startYear } = req.query;
-  console.log('startMonth', startMonth);
-  console.log('startYear', startYear);
+
   const userId = req.user._id;
   const monthReports = await MonthReportModel.find({ userId });
   console.log('monthReports.length', monthReports.length);
