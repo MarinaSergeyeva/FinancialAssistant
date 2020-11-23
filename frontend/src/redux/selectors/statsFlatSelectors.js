13dcf4f63c304fb10ddb2bf9452d9baa8e135918
Pa30p42 <hajiev93@gmail.com>
2020-11-23 14:42:15 +0300

    logic done

---
+++ frontend/src/redux/selectors/statsFlatSelectors.js
@@ -0,0 +1,5 @@
+const getGiftsForUnpacking = state => state.user.stats.giftsForUnpacking;
+
+const getSavingsPercentage = state => state.user.stats.savingsPercentage;
+
+export default { getGiftsForUnpacking, getSavingsPercentage };
