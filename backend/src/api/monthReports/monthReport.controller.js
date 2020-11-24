66c59c5fc7426a84cbd99e98d2efc2eb80777c8f
Marina Sergeyeva <morrrinad@gmail.com>
2020-11-24 11:16:41 +0200

    save

--- backend/src/api/monthReports/monthReport.controller.js
+++ backend/src/api/monthReports/monthReport.controller.js
@@ -7,11 +7,12 @@ const getMonthReport = catchAsync(async (req, res, next) => {
 
   const date = new Date();
 
-  const presentMonth = new Date(date.getFullYear(), date.getMonth() + 1, 1);
+  const presentMonth = new Date(date.getFullYear(), date.getMonth() + 1);
+  const presentMonth2 = new Date(startYear, startMonth);
   console.log('presentMonth', presentMonth);
-  const presentMonthInMS = Date.parse(presentMonth);
-  console.log('presentMonthInMS', presentMonthInMS);
-  const endMonth = Date(presentMonthInMS - 24 * 60 * 60 * 1000 * 365);
+  console.log('presentMonth2', presentMonth2);
+
+  const endMonth = new Date(date.getFullYear() - 1, date.getMonth());
   console.log('endMonth', endMonth);
 
   const userId = req.user._id;
@@ -20,7 +21,7 @@ const getMonthReport = catchAsync(async (req, res, next) => {
       $match: {
         userId,
         // reportDate: { $lte: startMonth },
-        reportDate: { $lte: presentMonth },
+        reportDate: { $lte: presentMonth, $gte: endMonth },
         //   reportDate: { $gte: endMonth },
       },
     },
@@ -30,6 +31,30 @@ const getMonthReport = catchAsync(async (req, res, next) => {
     //     expenses: { $sum: '$amount' },
     //   },
     // },
+
+    db.test.aggregate([
+      {
+        $group: {
+          _id: {
+            $let: {
+              vars: {
+                local_time: { $subtract: ['$date', 10800000] },
+              },
+              in: {
+                $concat: [
+                  { $substr: [{ $year: '$$local_time' }, 0, 4] },
+                  '-',
+                  { $substr: [{ $month: '$$local_time' }, 0, 2] },
+                  '-',
+                  { $substr: [{ $dayOfMonth: '$$local_time' }, 0, 2] },
+                ],
+              },
+            },
+          },
+          count: { $sum: 1 },
+        },
+      },
+    ]),
   ]);
   console.log('monthReports.length', monthReports.length);
   res.status(201).json({
