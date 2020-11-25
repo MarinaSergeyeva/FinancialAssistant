1faaaaa7d104db69535704333b93b445727d11d5
Marina-Melikhova <marinamelihova29@gmail.com>
2020-11-25 20:43:10 +0200

    done get transactions

--- backend/src/api/transactions/transaction.controller.js
+++ backend/src/api/transactions/transaction.controller.js
@@ -1,8 +1,7 @@
-const catchAsync = require('../../utils/catchAsync');
 const { TransactionModel } = require('./transaction.model');
 const { expensesCategories } = require('../dataset');
 
-const createTransaction = catchAsync(async (req, res, next) => {
+const createTransaction = async (req, res, next) => {
   const { body, user } = req;
   const userId = user._id;
   const info = { ...body, userId };
@@ -11,7 +10,7 @@ const createTransaction = catchAsync(async (req, res, next) => {
     status: 'success',
     transaction: newTransaction,
   });
-});
+};
 
 const getTransactionCategories = (req, res) => {
   const categories = Object.values(expensesCategories);
@@ -20,7 +19,29 @@ const getTransactionCategories = (req, res) => {
   });
 };
 
+const getListExpensesMonth = async (req, res, next) => {
+  const { user } = req;
+  const { month, year } = req.query;
+  const startDate = new Date(year, month - 1);
+  const endDate = new Date(year, month);
+  const transactionsDB = await TransactionModel.find({
+    userId: user._id,
+    transactionDate: { $gte: startDate, $lt: endDate },
+    type: 'EXPENSE',
+  });
+  const transactions = transactionsDB.map(
+    ({ amount, category, comment, transactionDate }) => ({
+      amount,
+      category,
+      comment,
+      transactionDate,
+    }),
+  );
+  return res.json(transactions);
+};
+
 module.exports = {
   createTransaction,
   getTransactionCategories,
+  getListExpensesMonth,
 };
