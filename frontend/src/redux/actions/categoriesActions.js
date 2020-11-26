d27e3fad072b6b5930b906b5c077a872006a7cd7
Marina Sergeyeva <morrrinad@gmail.com>
2020-11-26 16:25:13 +0200

    redux categories fixed

---
+++ frontend/src/redux/actions/categoriesActions.js
@@ -0,0 +1,24 @@
+import constants from '../constants/categoriesConstants';
+
+const categoriesRequest = () => ({
+  type: constants.CATEGORIES_REQUEST,
+});
+
+const categoriesSuccess = categories => {
+  console.log('categories', categories);
+  return {
+    type: constants.CATEGORIES_SUCCESS,
+    payload: categories,
+  };
+};
+
+const categoriesError = error => ({
+  type: constants.CATEGORIES_ERROR,
+  payload: error,
+});
+
+export default {
+  categoriesRequest,
+  categoriesSuccess,
+  categoriesError,
+};
