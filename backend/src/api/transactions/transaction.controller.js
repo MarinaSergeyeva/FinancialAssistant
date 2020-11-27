15b506bdbb631017dab9b310dbf8ed1c6f783b60
Marina-Melikhova <marinamelihova29@gmail.com>
2020-11-27 17:26:43 +0200

    edit gerTransactions

--- backend/src/api/transactions/transaction.controller.js
+++ backend/src/api/transactions/transaction.controller.js
@@ -21,22 +21,61 @@ const getTransactionCategories = (req, res) => {
 
 const getListExpensesMonth = async (req, res, next) => {
   const { user } = req;
-  const { month, year } = req.query;
+  let { page, limit, month, year } = req.query;
+  let pagination = true;
+  if (!limit) {
+    pagination = false;
+    limit = 10;
+  }
+  if (!page) {
+    page = 1;
+  }
+  const today = new Date();
+  if (!year) {
+    year = today.getFullYear();
+  }
+  if (!month) {
+    month = today.getMonth();
+  }
   const startDate = new Date(year, month - 1);
   const endDate = new Date(year, month);
+  // select:   'title date author',
   const transactionsDB = await TransactionModel.find({
     userId: user._id,
     transactionDate: { $gte: startDate, $lt: endDate },
     type: 'EXPENSE',
   });
   const transactions = transactionsDB.map(
-    ({ amount, category, comment, transactionDate }) => ({
+    ({ _id, amount, category, comment, transactionDate }) => ({
+      _id,
       amount,
       category,
       comment,
       transactionDate,
     }),
   );
+  var query = {};
+  var options = {
+    select: 'title date author',
+    sort: { date: -1 },
+    populate: 'author',
+    lean: true,
+    offset: 20,
+    limit: 10,
+  };
+  // const transactions = await TransactionModel.paginate(query, {
+  //   page,
+  //   limit,
+  //   pagination,
+  // });
+  // async getContacts(sub, pagination = false, page = 1, limit = 10) {
+  //   let query = {};
+  //   if (sub) {
+  //     query = { subscription: sub };
+  //   }
+  //   const { docs } = await this.db.paginate(query, { page, limit, pagination });
+  //   return docs;
+  // }
   return res.json(transactions);
 };
 
