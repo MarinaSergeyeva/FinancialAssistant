3475bef4cabc730efcb515fd66922847e82a9d1f
Anastasia-Dmv <Vesta50000@gmail.com>
2020-11-27 10:01:25 +0200

    x

--- backend/tests/cron.spec.js
+++ backend/tests/cron.spec.js
@@ -1,12 +1,9 @@
 require('dotenv').config({ path: './.env' });
 const cron = require('node-cron');
 const mongoose = require('mongoose');
-
-const jwt = require('jsonwebtoken');
 const { CrudServer } = require('../src/server');
 const request = require('supertest');
 const { assert, expect } = require('chai');
-const faker = require('faker');
 
 const { monthlyUpdatesUsersInfo } = require('../src/api/cron/cronUpdateInfo');
 const UserModel = require('../src/api/users/user.model');
@@ -15,9 +12,7 @@ const {
   TransactionModel,
 } = require('../src/api/transactions/transaction.model');
 
-mongoose.Promise = global.Promise;
-
-describe(' #monthlyUpdatesUsersInfo  updating  info via cron tests suite', () => {
+describe(' #monthlyUpdatesUsersInfo  updating  via cron tests suite', () => {
   let server;
 
   before(async () => {
@@ -25,42 +20,47 @@ describe(' #monthlyUpdatesUsersInfo  updating  info via cron tests suite', () =>
     await authServer.startForTest();
     server = authServer.server;
   });
+  describe('monthlyUpdatesUsersInfo #CronNNNN', () => {
+    context('#update ', () => {
+      let fakeTransaction, newUser, fakeMonthReport;
+      before(async () => {
+        newUser = await UserModel.create({
+          username: 'test509userUpdate',
+          email: 'test300Cron@email.com',
+          passwordHash: '200password_hash',
+          balance: 4,
+          flatPrice: 3000000,
+          flatSquareMeters: 110,
+          totalSalary: 1,
+          passiveIncome: 5,
+          incomePercentageToSavings: 90,
+        });
+        console.log('newUser', newUser);
+        // });
+        fakeTransaction = await TransactionModel.create({
+          amount: 1500,
+          transactionDate: Date.now(),
+          category: 'Развлечения',
+          type: 'EXPENSE',
+          userId: newUser._id,
+        });
 
-  context('#update ', () => {
-    before(async () => {
-      newUser = await UserModel.create({
-        username: 'test300Cron',
-        email: 'test300Cron@email.com',
-        passwordHash: '200password_hash',
-        balance: 4,
-        flatPrice: 3000000,
-        flatSquareMeters: 110,
-        totalSalary: 1,
-        passiveIncome: 5,
-        incomePercentageToSavings: 90,
+        await monthlyUpdatesUsersInfo();
+      });
+      console.log('newUser', newUser);
+      after(async () => {
+        await UserModel.deleteOne({ username: newUser.username });
+        await TransactionModel.deleteOne({ _id: fakeTransaction._id });
+        await monthReportModel.deleteOne({ userId: newUser._id });
       });
 
-      const fakeTransaction = await TransactionModel.create({
-        amount: 1500,
-        transactionDate: Date.now(),
-        category: 'Развлечения',
-        type: 'EXPENSE',
+      it('should increment balance', async () => {
+        const updatedUser = await UserModel.findById(newUser._id);
+        assert.equal(
+          updatedUser.balance,
+          newUser.balance + newUser.totalSalary + newUser.passiveIncome,
+        );
       });
-      await monthlyUpdatesUsersInfo();
-    });
-
-    after(async () => {
-      await userModel.deleteOne({ username: 'test200Cron' });
-      await TransactionModel.deleteOne({ _id: fakeTransaction._id });
-      await monthReportModel.deleteOne({ userId: newUser._id });
-    });
-
-    it('should increment balance', async () => {
-      const updatedUser = await UserModel.findById(newUser._id);
-      assert.equal(
-        updatedUser.balance,
-        newUser.balance + newUser.totalSalary + newUser.passiveIncome,
-      );
     });
   });
 });
