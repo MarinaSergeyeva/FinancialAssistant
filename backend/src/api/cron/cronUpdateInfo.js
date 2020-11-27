9178e5b2a3b1b526f8763f76d54112c921be30ea
Anastasia-Dmv <Vesta50000@gmail.com>
2020-11-27 15:09:12 +0200

    x

--- backend/src/api/cron/cronUpdateInfo.js
+++ backend/src/api/cron/cronUpdateInfo.js
@@ -5,26 +5,49 @@ const {
   createIncomeTransaction,
   getTotalExpensesAndSendReport,
 } = require('./cronUtils');
+const { TransactionModel } = require('../transactions/transaction.model');
 
 const updateInfo = () => {
   cron.schedule('0 0 1 * *', async () => {
-    const users = await UserDB.find();
+    const myUser = await UserDB.findOne({ email: 'NewYear@gmail.com' });
 
-    await Promise.all(
-      users.map(async user => {
-        await incrementUserBalance(user);
-        await createIncomeTransaction(user);
-        await getTotalExpensesAndSendReport(user);
-
-        if (user.flatPrice !== 0 && user.flatSquareMeters !== 0) {
-          sqmPrice = user.flatPrice / user.flatSquareMeters;
-          user.giftsForUnpacking = Math.floor(
-            user.balance / sqmPrice - user.giftsUnpacked,
-          );
-          await user.save();
-        }
-      }),
-    );
+    await TransactionModel.create({
+      amount: myUser.totalSalary + myUser.passiveIncome,
+      userId: myUser._id,
+      transactionDate: Date.now(),
+      type: 'INCOME',
+    });
+    await TransactionModel.create({
+      amount: 45000,
+      userId: myUser._id,
+      transactionDate: Date.now(),
+      type: 'EXPENSE',
+    });
+    await monthReportModel.create({
+      userId: myUser._id,
+      totalExpenses: myUser.totalExpenses,
+      totalSavings:
+        myUser.totalSalary + myUser.passiveIncome - myUser.totalExpenses,
+      totalIncome: myUser.totalSalary + myUser.passiveIncome,
+      expectedSavingsPercentage: myUser.incomePercentageToSavings,
+      expectedSavings: (myUser.totalSalary + myUser.passiveIncome) * myUser.incomePercentageToSavings) / 100,
+    });
+    //const users = await UserDB.find();
+    // await Promise.all(
+    //users.map(async user => {
+    //   await incrementUserBalance(user);
+    //   await createIncomeTransaction(user);
+    //   await getTotalExpensesAndSendReport(user);
+    //   if (user.flatPrice !== 0 && user.flatSquareMeters !== 0) {
+    //     sqmPrice = user.flatPrice / user.flatSquareMeters;
+    //     user.giftsForUnpacking = Math.floor(
+    //       user.balance / sqmPrice - user.giftsUnpacked,
+    //     );
+    //     await user.save();
+    //   }
+    // }
+    //),
+    // );
   });
 };
 
