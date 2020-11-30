b8a6c12f62b214ee9c4f47575e1116dc7df8279d
Pa30p42 <hajiev93@gmail.com>
2020-11-30 15:20:23 +0300

    merging

--- frontend/src/redux/reducers/statsReduce.js
+++ frontend/src/redux/reducers/statsReduce.js
@@ -13,6 +13,13 @@ const initialState = {
   },
 };
 
+const countPercentage = state => {
+  const result =
+    Math.floor((state.giftsUnpacked / state.totalSquareMeters) * 100) / 100;
+
+  return result;
+};
+
 const stats = (state = initialState.stats, { type, payload }) => {
   switch (type) {
     case statsConstants.GET_STATS_SUCCESS:
@@ -23,6 +30,7 @@ const stats = (state = initialState.stats, { type, payload }) => {
         ...{
           ...payload,
           savingsInSquareMeters: state.savingsInSquareMeters + 1,
+          savingsPercentage: countPercentage(state),
         },
       };
 
