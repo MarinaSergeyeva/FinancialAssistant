34804f6d8229b2afd9cef6135c2e3352d2bef22d
Marina Sergeyeva <morrrinad@gmail.com>
2020-12-03 12:32:02 +0200

    public route fixed

--- frontend/src/components/CustomRoutes/PublicRoute.js
+++ frontend/src/components/CustomRoutes/PublicRoute.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import withRouterHOC from './withRouterHOC';
-import useReduxState from '../../hooks/useReduxState';
 
 const PublicRoute = ({
   component: Component,
   isAuthenticated,
   ...routeProps
 }) => {
-  const { userInfo } = useReduxState();
-  const { flatPrice } = userInfo;
   return (
     <Route
       {...routeProps}
       render={props =>
         isAuthenticated && routeProps.restricted ? (
-          !flatPrice ? (
-            <Redirect to="/plan" />
-          ) : (
-            <Redirect to="/plan" />
-          )
+          <Redirect to="/plan" />
         ) : (
           <Component {...props} />
         )
