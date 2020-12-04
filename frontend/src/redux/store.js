7dcf90fd3d570425ac408c1eb2e9b266a9a7af97
Marina Sergeyeva <morrrinad@gmail.com>
2020-12-04 14:50:10 +0200

    redux-devtools changed developmentOnly

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -15,7 +15,7 @@
 // export default store;
 
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/root';
 import { persistStore } from 'redux-persist';
