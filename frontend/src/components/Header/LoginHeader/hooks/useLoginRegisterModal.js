3a793b28a349c06cb4170422fb02b58ef97f08e9
Marina Sergeyeva <morrrinad@gmail.com>
2020-12-04 18:19:46 +0200

    save

--- frontend/src/components/Header/LoginHeader/hooks/useLoginRegisterModal.js
+++ frontend/src/components/Header/LoginHeader/hooks/useLoginRegisterModal.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 const useLoginRegisterModal = () => {
   const [isShowRegistration, setIsShowRegistration] = useState(false);
