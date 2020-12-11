ed5e451f1a09e8b468204b60d937eb065f187ffd
Marina-Melikhova <marinamelihova29@gmail.com>
2020-12-11 13:43:11 +0200

    look like that's it

--- frontend/src/components/Expense/hooks/useExpense.js
+++ frontend/src/components/Expense/hooks/useExpense.js
@@ -1,7 +1,16 @@
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  transactionOperations,
+  categoriesOperations,
+} from '../../../redux/operations';
+import transactionActions from '../../../redux/actions/transactionActions';
 import { DateTime } from 'luxon';
-import { useState } from 'react';
+import useReduxState from '../../../hooks/useReduxState';
 
-const useExpense = () => {
+const useExpense = date => {
+  const { userTransactions } = useReduxState();
+  const { expenses } = userTransactions;
   const [page, setPage] = useState(1);
 
   const getDate = transactionDate => {
@@ -13,12 +22,49 @@ const useExpense = () => {
     setPage(page + 1);
   };
 
-  return {
-    page,
-    setPage,
-    loadMore,
-    getDate,
-  };
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(categoriesOperations.getCategories());
+    // eslint-disable-next-line
+  }, []);
+  const [countTransactions, setCountTransactions] = useState(0);
+
+  useEffect(() => {
+    if (expenses.length > 0) {
+      const lastPage = Math.ceil(expenses.length / 10);
+      const lastPart = expenses.length - (lastPage - 1) * 10;
+      setCountTransactions(lastPart);
+      if (page <= lastPage) {
+        const prevDate = new Date(expenses[0].transactionDate);
+        if (
+          prevDate.getMonth() === date.getMonth() &&
+          prevDate.getFullYear() === date.getFullYear()
+        ) {
+          if (lastPage > page) {
+            setPage(lastPage);
+          }
+          return;
+        } else {
+          dispatch(transactionActions.resetTransactionsExpense());
+          setPage(1);
+        }
+      }
+    }
+    const getData = async () => {
+      const countRows = await dispatch(
+        transactionOperations.getTransactionsExpense(
+          date.getMonth() + 1,
+          date.getFullYear(),
+          page,
+        ),
+      );
+      setCountTransactions(countRows);
+    };
+    getData();
+    // eslint-disable-next-line
+  }, [page, date]);
+
+  return { countTransactions, loadMore, getDate };
 };
 
 export default useExpense;
